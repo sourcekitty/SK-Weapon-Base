c2e1c42bd04a543acfaf38af70984b7de1e6c940
source kitty <154566733+sourcekitty@users.noreply.github.com>
2025-10-09 09:40:12 -0600

    Updated to new checker

--- lua/autorun/client/sk_basechecker_addonname.lua
+++ lua/autorun/client/sk_basechecker_addonname.lua
@@ -1,8 +1,10 @@
 local hasbase = false
 local nbv = 1 --Needed Major Base Version, Only change if you updated your addon & check what version the base is on.
 if SK_HAS_BASE then hasbase = true end
-hook.Add("InitPostEntity", "MissingAddonMessage" .. math.random(0, 1000), function() 
+hook.Add("InitPostEntity", "MissingAddonMessage" .. math.random(0, 1000), function()
+    if SK_BASE_MESSAGE then return end
     if not hasbase then
+        SK_BASE_MESSAGE = true
         Derma_Message(
             "You need the SK Weapon Base or weapons will not work correctly.\nGo download it.", 
             "SK Addon Checker", 
@@ -11,14 +13,17 @@ hook.Add("InitPostEntity", "MissingAddonMessage" .. math.random(0, 1000), functi
     end
 end)
 if not hasbase then return end
-hook.Add("InitPostEntity", "IncorrectFileVersion" .. math.random(0, 1000), function() 
+hook.Add("InitPostEntity", "IncorrectFileVersion" .. math.random(0, 1000), function()
+    if SK_BASE_MESSAGE then return end
     if nbv < SK_BASE_VERSION then
+        SK_BASE_MESSAGE = true
         Derma_Message(
-            "You may need a newer version of SK Weapon Base.\nNote to dev: Update your addon", 
+            "You may need a newer version of SK Weapon Base.\nNote to dev: Update your addon.", 
             "SK Update Checker", 
             "ok, bugger off."
         )
     elseif nbv > SK_BASE_VERSION then
+        SK_BASE_MESSAGE = true
         Derma_Message(
             "You may need a older version of SK Weapon Base.\nSome things might break.", 
             "SK Update Checker", 
